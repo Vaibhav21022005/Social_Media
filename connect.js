cb7c5d6b885771ade2772df65e11fa5214f76f50
Vaibhav Dhone <donevaibhav785@gmail.com>
2026-03-19 18:49:07 +0530

    last change

--- connect.js
+++ connect.js
@@ -1,10 +1,13 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Vaibhav@21",
-  database: "social_media",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Vaibhav@21",
+  database: process.env.DB_NAME || "social_media",
 });
 
 // connect to database
